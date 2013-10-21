bb60c8ebc181b07c03baf1a794b8cf0ed2a7709e
Claudio Mignanti <c.mignanti@gmail.com>
2013-10-21 21:26:48 +0200

    grunt cordovacli

--- phone-gap/Gruntfile.js
+++ phone-gap/Gruntfile.js
@@ -16,11 +16,47 @@ module.exports = function(grunt) {
     changelog: {
       dest: '../CHANGELOG.md'
     },
+    cordovacli: {
+      options: {
+          path: '.'
+      },
+      add_platform: {
+          options: {
+              command: 'platform',
+              action: 'add',
+              platforms: ['ios', 'android']
+          }
+      },
+      add_plugins: {
+          options: {
+              command: 'plugin',
+              action: 'add',
+              plugins: [
+                  //'bluetoothserial',
+                  'console',
+                  'dialogs'
+              ]
+          }
+      },
+      build_android: {
+          options: {
+              command: 'build',
+              platforms: ['android']
+          }
+      },
+      emulate_android: {
+          options: {
+              command: 'emulate',
+              platforms: ['android']
+          }
+      }
+    }
   });
 
   grunt.registerTask('default', ['connect']);
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-cordovacli');
 
 };
